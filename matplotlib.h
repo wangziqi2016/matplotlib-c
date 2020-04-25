d1372adc3a0aa51f32d1385b8fe52aaf7b76a3c8
Ziqi Wang <ziqiw@mit.edu>
2020-04-24 20:52:53 -0400

    Adding fstr_cut

--- matplotlib.h
+++ matplotlib.h
@@ -9,4 +9,11 @@
 #include <string.h>
 #include <stdint.h>
 
+//* fstr_* - String processing of floating point numbers
+
+#define FSTR_CUT_AFTER_RADIX     0  // Leave certain number of digits after the radix point
+#define FSTR_CUT_SIG_DIGITS      1  // Significant digits
+
+char *fstr_cut(double num, int type, int arg); // Cutting the string in different ways
+
 #endif
