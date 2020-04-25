737773c8cf21ed0807ae4f46403cf41df432e6c5
Ziqi Wang <ziqiw@mit.edu>
2020-04-24 20:40:43 -0400

    Adding basic includes

---
+++ matplotlib.h
@@ -0,0 +1,12 @@
+
+#ifndef _MATPLOTLIB_H
+#define _MATPLOTLIB_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <math.h>
+#include <string.h>
+#include <stdint.h>
+
+#endif
