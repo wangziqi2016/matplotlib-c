08d5ba3a28f939e250e7a644cdcc7bd3e70a55b3
Ziqi Wang <ziqiw@mit.edu>
2020-04-25 17:46:30 -0400

    Adding test_fp_print

--- test-matplotlib.c
+++ test-matplotlib.c
@@ -1,6 +1,20 @@
 
 #include "matplotlib.h"
 
+void test_fp_print() {
+  printf("========== test_fp_print ==========\n");
+  char *buf = NULL;
+  printf("Step: Test number %f using non-neg frac count\n");
+  for(int i = 0;i < 8;i++) {
+    buf = fp_print(12345.678912, i);
+    printf("")
+    free(buf);
+  }
+
+  printf("Pass\n");
+  return;
+}
+
 int main() {
   printf("========== test-matplotlib ==========\n");
 
