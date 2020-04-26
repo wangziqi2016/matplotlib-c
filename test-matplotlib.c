41afb1716b258efd721ff1f07b4e7c3fce632e8c
Ziqi Wang <ziqiw@mit.edu>
2020-04-25 20:31:42 -0400

    Adding test_fp_trim

--- test-matplotlib.c
+++ test-matplotlib.c
@@ -19,6 +19,25 @@ void test_fp_power10() {
   return;
 }
 
+void test_fp_trim() {
+  printf("========== test_fp_trim ==========\n");
+  const double num = 12345.678912;
+  printf("Step 1: Use %f and fp_printf() for testing\n", num);
+  for(int i = -6;i < 10;i++) {
+    char *buf = fp_print(num, i);
+    // Note: Must use two separate printf, since we need to evaluate trim after the print
+    printf("num = \"%s\"", buf);
+    printf(", after trim = \"%s\"\n", fp_rtrim(buf));
+    free(buf);
+  }
+  printf("Step 2: Corner cases\n");
+  char buf[32], buf2[32];
+  strcpy(buf, "0.00000"); strcpy(buf2, "0.00000");
+  printf("num = \"%s\", after trim = \"%s\"\n", buf, fp_rtrim(buf2));
+  printf("Pass\n");
+  return;
+}
+
 void test_fp_print() {
   printf("========== test_fp_print ==========\n");
   char *buf = NULL;
@@ -42,6 +61,7 @@ void test_fp_print() {
 int main() {
   printf("========== test-matplotlib ==========\n");
   test_fp_power10();
+  test_fp_trim();
   test_fp_print();
   printf("All test passed!\n");
   return 0;
