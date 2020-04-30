1371cf1f9732d029a80e46b37202584694f181cd
Ziqi Wang <ziqiw@mit.edu>
2020-04-29 22:11:22 -0400

    Adding bar_set_type

--- matplotlib.h
+++ matplotlib.h
@@ -163,6 +163,7 @@ void bar_free(bar_t *bar);
 inline static uint32_t bar_get_color(bar_t *bar) { return bar->type->color; }
 inline static char bar_get_hatch(bar_t *bar) { return bar->type->hatch; }
 inline static bar_type_t *bar_get_type(bar_t *bar) { return bar->type; }
+inline static void bar_set_type(bar_t *bar, bar_type_t *type) { bar->type = type; }
 
 //* plot_t - Plotting function
 
