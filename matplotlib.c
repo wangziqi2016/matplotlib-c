cb99819a2ffeae7b5c9620b8e5eadded455e96b8
Ziqi Wang <ziqiw@mit.edu>
2020-05-10 20:45:57 -0400

    Adding help for set color scheme

--- matplotlib.c
+++ matplotlib.c
@@ -2264,6 +2264,12 @@ void parse_cb_set_color_scheme(parse_t *parse, plot_t *plot) {
   int next_type = parse_next_arg(parse);
   if(next_type == PARSE_ARG_NONE) {
     error_exit("Function \"set_color_scheme\" takes at least 1 argument\n");
+  } else if(next_type == PARSE_ARG_QMARK) {
+    parse_expect_char(parse, '?');
+    printf("[set_color_scheme] Usage: set_color_scheme [name]/[file name]\n");
+    printf("[set_color_scheme] Name is a string for built-in schemes; File name is a file name indicator"
+           " (beginning with '@'), which causes the scheme file being load\n");
+    return;
   }
   plot_param_t *param = &plot->param;
   // Free current one if there is one
