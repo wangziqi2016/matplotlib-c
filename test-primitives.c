a87305f8cb3f38ca7dbdc7c1a8f1632db50be38b
Ziqi Wang <675973885@qq.com>
2021-10-19 19:18:06 -0400

    Adding new test file

---
+++ test-primitives.c
@@ -0,0 +1,10 @@
+
+#include "primitives.h"
+
+int main() {
+  printf("========== %s ==========\n", __FILE__);
+
+  printf("Compiled %s %s\n", __DATE__, __TIME__);
+  printf("========================\n");
+  return 0;
+}
