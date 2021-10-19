3c1666d88bbcda1f7387e5b5521d39009472c08f
Ziqi Wang <675973885@qq.com>
2021-10-19 19:02:04 -0400

    Adding point_t

--- primitives.h
+++ primitives.h
@@ -4,8 +4,15 @@
 
 // This header file defines the primitives shapes that can be drawn
 
+// Position of a point. If this is given relative to a plot, it always starts at bottom left of the rectangular
 typedef struct {
+  double x;
+  double y;
+} point_t;
 
-}
+typedef struct {
+  double width;
+  double height;
+} plot_t;
 
 #endif
